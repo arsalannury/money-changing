8ee40725054d754b204234ad98be6c264d189bba
arsalannury <arsiiiland@yaho.com>
2021-11-24 23:14:15 +0330

    responsive order page

--- js/bank.js
+++ js/bank.js
@@ -267,7 +267,7 @@ function keyUpCheckName(){
 let minutes = 150;
 window.addEventListener('DOMContentLoaded',(e) => {
    showModalForSetUserName();
-//    setInterval(() => {minutes--; minutes === 0 ? location.replace('./index.html') : null},1000)
+   setInterval(() => {minutes--; minutes === 0 ? location.replace('./index.html') : null},1000)
 })
 
 buttonAddName.addEventListener('click',(e) => {
